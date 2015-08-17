e33f7502d01f20ea898b433921d1e847a00a86db
Lin, Forrest <forrest.lin@sap.com>
2015-08-17 16:48:47 +0800

    Initial Commit

---
+++ TestHidingTabbar/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TestHidingTabbar
+//
+//  Created by Lin, Forrest on 8/17/15.
+//  Copyright © 2015 Shi, Forrest. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TestHidingTabbar/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TestHidingTabbar
+//
+//  Created by Lin, Forrest on 8/17/15.
+//  Copyright © 2015 Shi, Forrest. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
